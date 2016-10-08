c91ee2f51ad44982cd534ccfacc040410a421ef8
Chapeiro <periklis93@yahoo.gr>
2016-10-08 23:46:35 +0200

    Initial implementation of a simple select (filter)
    Simple implementation of a filter (select) on GPGPU. The filter
    implemented is an unstable one, that is, the order of the results may be
    different that the initial one.

---
+++ select.cu
@@ -0,0 +1,189 @@
+#include <cstdio>
+#include <cstdint>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <cassert>
+
+using namespace std;
+
+const int N = 100000000;
+
+#define gpu(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true){
+    if (code != cudaSuccess) {
+        fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
+}
+
+#define WRAPSIZE (32)
+
+extern __shared__ int32_t s[];
+
+__global__ void unstable_select(int32_t *a_dev, int32_t *b_dev, int N){
+    int32_t *input  = (int32_t *) (s               );
+    int32_t *output = (int32_t *) (s +   blockDim.x);
+    int32_t *bcount = (int32_t *) (s + 3*blockDim.x);
+    int32_t *elems  = (int32_t *) (s + 4*blockDim.x);
+
+    int32_t i       = threadIdx.x;
+    int32_t laneid  = i % WRAPSIZE;
+    int32_t wrapid  = i / WRAPSIZE;
+    int32_t prevwrapmask = (1 << laneid) - 1;
+
+    int32_t filterout = 0;
+
+    if (i == 0) *elems = 0;
+    if (laneid == 0) bcount[wrapid] = 0;
+    __syncthreads();
+
+    // __shfl(devout, 0);
+    
+    //read from global memory
+    for (int j = 0 ; j < N ; j += blockDim.x){
+        bool predicate = false;
+        if (i+j < N){
+            input[i] = a_dev[i+j];
+
+            //compute predicate
+            predicate = input[i] < 50;
+        }
+        
+        //aggreagate predicate results
+        int32_t filter = __ballot(predicate); //filter now contains set bits only for threads
+
+        int32_t newpop = __popc(filter);
+
+        //compute position of result
+        if (predicate){
+            int32_t offset = filterout + __popc(filter & prevwrapmask);
+            output[offset] = input[i];
+        }
+
+        filterout += newpop;
+
+        if (filterout >= WRAPSIZE){
+            if (laneid == 0) bcount[wrapid] = atomicAdd(elems, WRAPSIZE);
+            int32_t elems_old              = bcount[wrapid];
+            b_dev[elems_old + laneid]      = output[i];
+            output[i]                      = output[i + blockDim.x];
+            filterout                     -= WRAPSIZE;
+        }
+    }
+
+    if (laneid == 0) bcount[wrapid] = atomicAdd(elems, filterout);
+    if (laneid < filterout) b_dev[bcount[wrapid] + laneid] = output[i];
+
+    __syncthreads();
+    if (i == 0) b_dev[*elems] = -1;
+}
+
+int32_t a[N];
+int32_t b[N];
+
+void stable_select_cpu(int32_t *a, int32_t *b, int N){
+    int i = 0;
+    for (int j = 0 ; j < N ; ++j) if (a[j] < 50) b[i++] = a[j];
+    b[i] = -1;
+}
+
+int main(){
+    srand(time(0));
+
+    for (auto &x: a) x = rand() % 100;
+    
+    // char *ad;
+    // int *bd;
+    // const int csize = N*sizeof(char);
+    // const int isize = N*sizeof(int);
+
+    double millis = 0;
+    {
+        auto start = chrono::high_resolution_clock::now();
+        stable_select_cpu(a, b, N);
+        auto end   = chrono::high_resolution_clock::now();
+        auto diff = end - start;
+
+        millis = chrono::duration<double, milli>(diff).count();
+        cout << millis << " ms" << endl;
+    }
+    int32_t *a_pinned, *a_dev;
+    int32_t *b_pinned, *b_dev;
+    cudaEvent_t start, stop, start1, stop1, start2, stop2;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventCreate(&start1);
+    cudaEventCreate(&stop1);
+    cudaEventCreate(&start2);
+    cudaEventCreate(&stop2);
+    dim3 dimBlock( 1024, 1 );
+    dim3 dimGrid( 1, 1 );
+
+    cudaMallocHost((void**)&a_pinned, N*sizeof(int32_t));
+    cudaMallocHost((void**)&b_pinned, N*sizeof(int32_t));
+
+    memcpy(a_pinned, a, N*sizeof(int32_t));
+
+    gpu(cudaMalloc( (void**)&a_dev, N*sizeof(int32_t)));
+    gpu(cudaMalloc( (void**)&b_dev, N*sizeof(int32_t)));
+
+    cudaEventRecord(start);
+    gpu(cudaMemcpy( a_dev, a_pinned, N*sizeof(int32_t), cudaMemcpyDefault));
+    
+    cudaEventRecord(start1);
+    unstable_select<<<dimGrid, dimBlock, (4 * dimBlock.x + 1)*sizeof(int32_t)>>>(a_pinned, b_pinned, N);
+    cudaEventRecord(stop1);
+    cudaEventRecord(start2);
+    unstable_select<<<dimGrid, dimBlock, (4 * dimBlock.x + 1)*sizeof(int32_t)>>>(a_dev, b_dev, N);
+    cudaEventRecord(stop2);
+    gpu(cudaMemcpy( a_pinned, b_dev, N*sizeof(int32_t), cudaMemcpyDefault));
+    cudaEventRecord(stop);
+    gpu(cudaFree(a_dev));
+    gpu(cudaFree(b_dev));
+
+    cudaEventSynchronize(stop);
+
+    int results1 = N;
+    for (int i = 0 ; i < N ; ++i) {
+        if (b_pinned[i] == -1) {
+            results1 = i;
+            break;
+        }
+    }
+    int results = N;
+    for (int i = 0 ; i < N ; ++i) {
+        if (a_pinned[i] == -1) {
+            results = i;
+            break;
+        }
+    }
+    int results2 = N;
+    for (int i = 0 ; i < N ; ++i) {
+        if (b[i] == -1) {
+            results2 = i;
+            break;
+        }
+    }
+    cout << results << " " << results1 << " " << results2 << " " << a_pinned[4] << endl;
+
+    assert(results == results2);
+
+    cudaFreeHost(a_pinned);
+
+    cudaEventSynchronize(stop);
+    float milliseconds1 = 0;
+    cudaEventElapsedTime(&milliseconds1, start, stop);
+    cout << milliseconds1 << endl;
+    float milliseconds2 = 0;
+    cudaEventElapsedTime(&milliseconds2, start1, stop1);
+    cout << milliseconds2 << endl;
+    float milliseconds3 = 0;
+    cudaEventElapsedTime(&milliseconds3, start2, stop2);
+    cout << milliseconds3 << endl;
+
+    cout << endl;
+    cout << (millis - milliseconds2)/milliseconds1 << endl;
+    cout << (millis - milliseconds2)/milliseconds3 << endl;
+    return EXIT_SUCCESS;
+}
